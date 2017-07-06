4909d6e4e8536c1cd60f026191b94c7d71bc0795
lixinjie <lixj@asiainfo.com>
2017-07-06 10:16:20 +0800

    update

--- src/sample/java/org/eop/zookeeper/sample/ExistsMethod.java
+++ src/sample/java/org/eop/zookeeper/sample/ExistsMethod.java
@@ -24,18 +24,22 @@ public class ExistsMethod implements Watcher {
 	private ZooKeeper zk;
 	
 	public ExistsMethod() throws IOException, InterruptedException {
+		//ExistsMethod类实现了Watcher接口，这样自己就是一个Watcher
+		//在构造ZooKeeper时将自己传进去，当ZooKeeper连接成功时
+		//自己就可以收到通知，即process方法会被执行
 		zk = new ZooKeeper("127.0.0.1:2181", 3000, this);
 	}
 
 	@Override
 	public void process(WatchedEvent event) {
+		//该方法首次执行接收的是连接成功事件
 		System.out.println("事件线程：" + Thread.currentThread().getId());
 		System.out.println(this.getClass().getName());
 		System.out.print("路径：" + event.getPath());
 		System.out.print("，事件：" + event.getType());
 		System.out.print("，状态：" + event.getState());
 		System.out.println();
-		exists();
+		exists();//由于Watcher只能触发一次，这里每次都要重新设置一遍
 	}
 	
 	public void exists() {
